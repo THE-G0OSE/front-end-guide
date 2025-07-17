e50972916b4a4d394082a580c8a0ce6857e230df
THE_GOOSE <nimitos54345@gmail.com>
2025-07-18 00:23:52 +0300

    fix

--- src/widgets/canvas/ui/components/lights/ui/Lights.tsx
+++ src/widgets/canvas/ui/components/lights/ui/Lights.tsx
@@ -1,8 +1,8 @@
 const Lights = () => {
   return (
     <>
-    <ambientLight intesity={1.2} />
-    <directionalLight position={[100,100,60]} intesity={2}/>
+    <ambientLight intensity={1.2} />
+    <directionalLight position={[100,100,60]} intensity={2}/>
     </>
   )
 }
